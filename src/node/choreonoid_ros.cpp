139df31a1c7a275d2c636b4571261f035cb593d7
Shin'ichiro Nakaoka <s.nakaoka@aist.go.jp>
2019-11-12 00:47:32 +0900

    Stop the use of fmtlib

--- src/node/choreonoid_ros.cpp
+++ src/node/choreonoid_ros.cpp
@@ -2,12 +2,10 @@
 #include <cnoid/App>
 #include <cnoid/ExecutablePath>
 #include <cnoid/FileUtil>
-#include <fmt/format.h>
 #include <cstdlib>
 
 using namespace std;
 using namespace cnoid;
-using fmt::format;
 namespace filesystem = stdx::filesystem;
 
 int main(int argc, char **argv)
@@ -20,7 +18,7 @@ int main(int argc, char **argv)
     if(!pluginPathVariable){
         pluginPathList = rosPluginPath;
     } else {
-        pluginPathList = format("{0}{1}{2}", pluginPathVariable, PATH_DELIMITER, rosPluginPath);
+        pluginPathList = string(pluginPathVariable) + PATH_DELIMITER + rosPluginPath;
     }
 
     app.initialize("Choreonoid", "Choreonoid", pluginPathList.c_str());

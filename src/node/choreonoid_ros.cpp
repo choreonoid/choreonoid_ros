4482ce254ffc7fb65a67509791c7d53aed0811f4
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2020-03-25 17:40:16 +0900

    Move the initialization of ROS from ROSPlugin to Choreonoid's ROS node

--- src/node/choreonoid_ros.cpp
+++ src/node/choreonoid_ros.cpp
@@ -1,8 +1,12 @@
-#include "../util/ROSUtil.h"
 #include <cnoid/Config>
 #include <cnoid/App>
+#include <cnoid/PluginManager>
+#include <cnoid/Plugin>
 #include <cnoid/ExecutablePath>
 #include <cnoid/FileUtil>
+#include <ros/init.h>
+#include <ros/master.h>
+#include <ros/spinner.h>
 #include <regex>
 #include <cstdlib>
 
@@ -10,12 +14,11 @@ using namespace std;
 using namespace cnoid;
 namespace filesystem = stdx::filesystem;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     regex remapPattern("^.+:=.+$");
     vector<char*> args{ argv[0] };
-    auto& rosargs = cnoid::rosInitArguments();
-    rosargs.push_back(argv[0]);
+    vector<char*> rosargs{ argv[0] };
     for(int i=1; i < argc; ++i){
         if(regex_match(argv[i], remapPattern)){
             rosargs.push_back(argv[i]);
@@ -23,6 +26,19 @@ int main(int argc, char **argv)
             args.push_back(argv[i]);
         }
     }
+
+    int rosargc = rosargs.size();
+    char** rosargv = &rosargs.front();
+    ros::init(rosargc, rosargv, "choreonoid", ros::init_options::NoSigintHandler);
+
+    ros::master::setRetryTimeout(ros::WallDuration(1));
+    if(!ros::master::check()){
+        cerr << "Choreonoid's ROS node cannot be invoked because the ROS master is not found." << endl;
+        return 1;
+    }
+    std::unique_ptr<ros::AsyncSpinner> spinner;
+    spinner.reset(new ros::AsyncSpinner(0));
+    spinner->start();
 
     argc = args.size();
     argv = &args.front();
@@ -38,7 +54,21 @@ int main(int argc, char **argv)
     }
 
     app.initialize("Choreonoid", "Choreonoid", pluginPathList.c_str());
+
+    auto rosPlugin = PluginManager::instance()->findPlugin("ROS");
+    if(!rosPlugin){
+        cerr << "ROS plugin is not found." << endl;
+        return 1;
+    }
+    if(!rosPlugin->isActive()){
+        cerr << "ROS plugin is not active." << endl;
+        return 1;
+    }
+    
     app.exec();
+
+    ros::requestShutdown();
+    ros::waitForShutdown();
 
     return 0;
 }
--- src/plugin/ROSPlugin.cpp
+++ src/plugin/ROSPlugin.cpp
@@ -1,52 +1,34 @@
 #include "WorldROSItem.h"
 #include "BodyPublisherItem.h"
-#include "../util/ROSUtil.h"
 #include <cnoid/Plugin>
 #include <cnoid/MessageView>
 #include <ros/init.h>
-#include <ros/master.h>
-#include <ros/spinner.h>
 
 using namespace std;
 using namespace cnoid;
 
 class ROSPlugin : public Plugin
 {
-    std::unique_ptr<ros::AsyncSpinner> spinner;
-    
 public:
-    ROSPlugin() : Plugin("ROS") {
+    ROSPlugin() : Plugin("ROS")
+    {
         require("Body");
     }
   
     virtual bool initialize()
     {
         if(!ros::isInitialized()){
-            auto& args = rosInitArguments();
-            int argc = args.size();
-            char** argv = &args.front();
-            ros::init(argc, argv, "choreonoid", ros::init_options::NoSigintHandler);
-        }
-
-        if(!ros::master::check()){
             MessageView::instance()->putln(
-                MessageView::WARNING, "The ROS master is not found.");
+                "The ROS plugin cannot be used because ROS is not initialized. \n"
+                "Choreonoid must be invoked as a ROS node to make the ROS plugin available.",
+                MessageView::ERROR);
             return false;
         }
             
-        spinner.reset(new ros::AsyncSpinner(0));
-        spinner->start();
-
         WorldROSItem::initializeClass(this);
         BodyPublisherItem::initializeClass(this);
         
         return true;
-    }
-
-    virtual bool finalize()
-    {
-        ros::requestShutdown();
-        ros::waitForShutdown();
     }
 };
 

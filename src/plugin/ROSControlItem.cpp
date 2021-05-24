f9eccadddbd776bb9cfce15afe7338e2ff16e8f3
RyodoTanaka <groadpg@gmail.com>
2021-05-24 21:31:05 +0900

    Update to delete unnecessary part

--- src/plugin/ROSControlItem.cpp
+++ src/plugin/ROSControlItem.cpp
@@ -104,88 +104,23 @@ bool ROSControlItem::initialize(ControllerIO* io)
 
   try {
     // ros plugin loader //
-    if(rbt_hw_sim_loader_ == nullptr)
-      rbt_hw_sim_loader_ = make_shared<pluginlib::ClassLoader<RobotHWSim<cnoid::ControllerIO*>>>("choreonoid_ros", "hardware_interface::RobotHWSim<cnoid::ControllerIO*>");
-    if(rbt_hw_sim_ == nullptr) {
-      rbt_hw_sim_ = rbt_hw_sim_loader_->createInstance("hardware_interface/RobotHWCnoid");
-      // load hardware_interface  //
-      if(!rbt_hw_sim_->initSim(nh_, io_)) {
-        ss.str("");
-        ss << "Could not initialize robot simulation interface" << endl;
-        MessageView::instance()->put(ss.str(), MessageView::Error);
-      }
     }
     // register ros control manager //
-    if(manager_ == nullptr)
-      manager_ = make_shared<controller_manager::ControllerManager>(rbt_hw_sim_.get(), nh_);
-    else {
-      for(auto c : controller_names_) {
-        ros::ServiceClient sc = nh_.serviceClient<controller_manager_msgs::LoadController>("controller_manager/load_controller");
-        controller_manager_msgs::LoadController load;
-        load.request.name = c;
-        sc.call(load);
-      }
-    }
   } catch(pluginlib::LibraryLoadException &ex) {
     ss.str("");
     ss << "Failed to create robot simulation interface loader : " << ex.what() << endl;
     MessageView::instance()->put(ss.str(), MessageView::Error);
   }
   
-  ros::ServiceClient lcsc = nh_.serviceClient<controller_manager_msgs::ListControllers>("controller_manager/list_controllers");
-  controller_manager_msgs::ListControllers lc;
-  if(!lcsc.call(lc)) {
-    ss.str("");
-    ss << "Failed to lad cnoid::ROSControllerItem" << endl;
-    MessageView::instance()->put(ss.str(), MessageView::Normal);
-  } else { 
-    for(auto c : lc.response.controller) {
-      controller_names_.emplace_back(c.name);
-      cout << c << endl;
-    }
-  }
-  
   ss.str("");
   ss << "Loaded cnoid::ROSControlItem" << endl;
   MessageView::instance()->put(ss.str(), MessageView::Normal);
-
-  std::cout << "You are in initialize()" << std::endl;
   
   return true;
 }
 
 bool ROSControlItem::start(void)
 {
-  std::cout << "You are in start()" << std::endl;
-  
-  // using namespace std;
-  // using namespace cnoid;
-  // using namespace hardware_interface;
-  // stringstream ss;
-
-  // nh_ = ros::NodeHandle(namespace_);
-
-  // try {
-  //   // ros plugin loader //
-  //   rbt_hw_sim_loader_ = make_shared<pluginlib::ClassLoader<RobotHWSim<cnoid::ControllerIO*>>>("choreonoid_ros", "hardware_interface::RobotHWSim<cnoid::ControllerIO*>");
-  //   rbt_hw_sim_ = rbt_hw_sim_loader_->createInstance("hardware_interface/RobotHWCnoid");
-  //   // load hardware_interface  //
-  //   if(!rbt_hw_sim_->initSim(nh_, io_)) {
-  //     ss.str("");
-  //     ss << "Could not initialize robot simulation interface" << endl;
-  //     MessageView::instance()->put(ss.str(), MessageView::Error);
-  //   }
-  //   // register ros control manager //
-  //   manager_ = make_shared<controller_manager::ControllerManager>(rbt_hw_sim_.get(), nh_);
-  // } catch(pluginlib::LibraryLoadException &ex) {
-  //   ss.str("");
-  //   ss << "Failed to create robot simulation interface loader : " << ex.what() << endl;
-  //   MessageView::instance()->put(ss.str(), MessageView::Error);
-  // }
-
-  // ss.str("");
-  // ss << "Loaded cnoid::ROSControlItem" << endl;
-  // MessageView::instance()->put(ss.str(), MessageView::Normal);
   
   return true;
 }
@@ -219,19 +154,6 @@ void ROSControlItem::output(void)
 
 void ROSControlItem::stop(void)
 {
-  std::cout << "You are in stop()" << std::endl;
-
-  for(auto c : controller_names_) {
-    ros::ServiceClient sc = nh_.serviceClient<controller_manager_msgs::UnloadController>("controller_manager/unload_controller");
-    controller_manager_msgs::UnloadController unload;
-    unload.request.name = c;
-    sc.call(unload);
-  }
-
-  
-  rbt_hw_sim_.reset();
-  rbt_hw_sim_loader_.reset();
-//   manager_.reset();
 }
 
 } // namespace cnoid

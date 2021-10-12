8a0dd3fb6a71e842a0119b6a17f802d79c2a4105
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2021-10-13 06:25:01 +0900

    Migrate ROSPlugin.cpp from ROS to ROS2

--- src/plugin/ROS2Plugin.cpp
+++ src/plugin/ROS2Plugin.cpp
@@ -1,37 +1,24 @@
 #include "BodyROS2Item.h"
 #include "WorldROS2Item.h"
+#include "ROS2Plugin.hpp"
 #include "deprecated/BodyPublisherItem.h"
 #include <cnoid/MessageView>
-#include <cnoid/Plugin>
-#include <ros/init.h>
+
 
 using namespace std;
 using namespace cnoid;
 
-class ROS2Plugin : public Plugin
+ROS2Plugin::ROS2Plugin()
+    : Plugin("ROS2"), Node("choreonoid_ros2",rclcpp::NodeOptions())
 {
-public:
-    ROS2Plugin() : Plugin("ROS2")
-    {
-        require("Body");
-    }
-  
-    virtual bool initialize()
-    {
-        if(!ros::isInitialized()){
-            MessageView::instance()->putln(
-                "The ROS2 plugin cannot be used because ROS is not initialized. \n"
-                "Choreonoid must be invoked as a ROS node to make the ROS plugin available.",
-                MessageView::ERROR);
-            return false;
-        }
+  require("Body");
+}
+bool ROS2Plugin::initialize() {
+  WorldROS2Item::initializeClass(this);
+  BodyROS2Item::initializeClass(this);
+  BodyPublisherItem::initializeClass(this);
 
-        WorldROS2Item::initializeClass(this);
-        BodyROS2Item::initializeClass(this);
-        BodyPublisherItem::initializeClass(this);
-        
-        return true;
-    }
-};
+  return true;
+}
 
 CNOID_IMPLEMENT_PLUGIN_ENTRY(ROS2Plugin)

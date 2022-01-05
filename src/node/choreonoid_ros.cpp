c7928ab31684a4d7c23fc52bf31e003649781d3b
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2022-01-05 22:17:22 +0900

    Move the ROS initialization code from the main function to ROS plugin's customizeApplication function.

--- src/node/choreonoid_ros.cpp
+++ src/node/choreonoid_ros.cpp
@@ -1,78 +1,25 @@
-#include <cnoid/Config>
 #include <cnoid/App>
-#include <cnoid/ProjectManager>
-#include <cnoid/PluginManager>
-#include <cnoid/Plugin>
-#include <cnoid/ExecutablePath>
-#include <cnoid/FileUtil>
-#include <ros/init.h>
-#include <ros/master.h>
-#include <ros/spinner.h>
-#include <regex>
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 using namespace cnoid;
-namespace filesystem = stdx::filesystem;
 
 int main(int argc, char** argv)
 {
-    regex remapPattern("^.+:=.+$");
-    vector<char*> args{ argv[0] };
-    vector<char*> rosargs{ argv[0] };
-    for(int i=1; i < argc; ++i){
-        if(regex_match(argv[i], remapPattern)){
-            rosargs.push_back(argv[i]);
-        } else {
-            args.push_back(argv[i]);
-        }
-    }
+    cnoid::App app(argc, argv, "Choreonoid-ROS", "Choreonoid");
 
-    int rosargc = rosargs.size();
-    char** rosargv = &rosargs.front();
-    ros::init(rosargc, rosargv, "choreonoid", ros::init_options::NoSigintHandler);
-
-    // The setRetryTimeout function does not seem to affect the check function.
-    // The following code is currently disabled because there seems no special reason
-    // to use it other than to specify the timeout for the check function.
-    //
-    // ros::master::setRetryTimeout(ros::WallDuration(1));
-    
-    if(!ros::master::check()){
-        cerr << "Choreonoid's ROS node cannot be invoked because the ROS master is not found." << endl;
-        return 1;
-    }
-    std::unique_ptr<ros::AsyncSpinner> spinner;
-    spinner.reset(new ros::AsyncSpinner(0));
-    spinner->start();
-
-    argc = args.size();
-    argv = &args.front();
-    cnoid::App app(argc, argv);
-    app.initialize("Choreonoid-ROS", "Choreonoid");
-    ProjectManager::instance()->loadBuiltinProject(":/Base/project/layout.cnoid");
-
-    auto pm = PluginManager::instance();
-    auto rosPlugin = pm->findPlugin("ROS");
-    if(!rosPlugin){
-        auto& errorMessage = pm->getErrorMessage("ROS");
-        if(errorMessage.empty()){
-            cerr << "ROS plugin is not found." << endl;
-        } else {
-            cerr << "ROS plugin cannot be loaded.\n";
-            cerr << errorMessage << endl;
+    if(!app.requirePluginToCustomizeApplication("ROS")){
+        if(app.error() == App::PluginNotFound){
+            auto message = app.errorMessage();
+            if(message.empty()){
+                cerr << "ROS plugin is not found." << endl;
+            } else {
+                cerr << "ROS plugin cannot be loaded.\n";
+                cerr << message << endl;
+            }
         }
         return 1;
     }
-    if(!rosPlugin->isActive()){
-        cerr << "ROS plugin is not active." << endl;
-        return 1;
-    }
-    
-    int returnCode = app.exec();
-
-    ros::requestShutdown();
-    ros::waitForShutdown();
-
-    return returnCode;
+        
+    return app.exec();
 }
--- src/plugin/ROSPlugin.cpp
+++ src/plugin/ROSPlugin.cpp
@@ -3,27 +3,54 @@
 #include "ROSControlItem.h"
 #include "deprecated/BodyPublisherItem.h"
 #include <cnoid/Plugin>
-#include <cnoid/MessageView>
+#include <cnoid/AppCustomizationUtil>
+#include <cnoid/MessageManager>
 #include <ros/init.h>
+#include <ros/master.h>
+#include <ros/spinner.h>
+#include <memory>
+#include <iostream>
 
 using namespace std;
 using namespace cnoid;
 
 class ROSPlugin : public Plugin
 {
+    std::unique_ptr<ros::AsyncSpinner> spinner;
+    
 public:
     ROSPlugin() : Plugin("ROS")
     {
         require("Body");
     }
+
+    bool customizeApplication(AppCustomizationUtil& app) override
+    {
+        ros::init(app.argc(), app.argv(), "choreonoid", ros::init_options::NoSigintHandler);
+        
+        // The setRetryTimeout function does not seem to affect the check function.
+        // The following code is currently disabled because there seems no special reason
+        // to use it other than to specify the timeout for the check function.
+        //
+        // ros::master::setRetryTimeout(ros::WallDuration(1));
+        
+        if(!ros::master::check()){
+            cerr << "Choreonoid's ROS node cannot be invoked because the ROS master is not found." << endl;
+            return false;
+        }
+
+        spinner.reset(new ros::AsyncSpinner(0));
+        spinner->start();
+
+        return true;
+    }
   
-    virtual bool initialize()
+    virtual bool initialize() override
     {
         if(!ros::isInitialized()){
-            MessageView::instance()->putln(
+            MessageManager::master()->putError(
                 "The ROS plugin cannot be used because ROS is not initialized. \n"
-                "Choreonoid must be invoked as a ROS node to make the ROS plugin available.",
-                MessageView::ERROR);
+                "Choreonoid must be invoked as a ROS node to make the ROS plugin available.\n");
             return false;
         }
             
@@ -32,6 +59,14 @@ public:
         ROSControlItem::initializeClass(this);
         BodyPublisherItem::initializeClass(this);
         
+        return true;
+    }
+
+    virtual bool finalize() override
+    {
+        ros::requestShutdown();
+        ros::waitForShutdown();
+        spinner.reset();
         return true;
     }
 };

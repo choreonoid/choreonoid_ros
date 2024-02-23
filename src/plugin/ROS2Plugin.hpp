7a6d0c7aca75a9e1f5816cc43e6f9962a8cd075e
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-23 12:53:52 +0900

    Stop using ROS2Plugin as ROS 2 node

---
+++ src/plugin/ROS2Plugin.hpp
@@ -0,0 +1,14 @@
+#ifndef CHOREONOID_ROS2__ROS2PLUGIN_HPP_
+#define CHOREONOID_ROS2__ROS2PLUGIN_HPP_
+
+#include <cnoid/Plugin>
+#include <rclcpp/rclcpp.hpp>
+
+class ROS2Plugin : public cnoid::Plugin
+{
+public:
+    ROS2Plugin();
+    virtual bool initialize();
+};
+
+#endif  // CHOREONOID_ROS2__ROS2PLUGIN_HPP_

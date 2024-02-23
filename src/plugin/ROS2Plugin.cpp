fcd23c764e3f6b840aa84e28861d45373998cf26
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-02-23 12:53:52 +0900

    Stop using ROS2Plugin as ROS 2 node

---
+++ src/plugin/ROS2Plugin.cpp
@@ -0,0 +1,24 @@
+#include "ROS2Plugin.hpp"
+#include "BodyROS2Item.h"
+#include "WorldROS2Item.h"
+#include "deprecated/BodyPublisherItem.h"
+#include <cnoid/MessageView>
+
+
+using namespace std;
+using namespace cnoid;
+
+ROS2Plugin::ROS2Plugin()
+    : Plugin("ROS2")
+{
+    require("Body");
+}
+bool ROS2Plugin::initialize()
+{
+    WorldROS2Item::initializeClass(this);
+    BodyROS2Item::initializeClass(this);
+
+    return true;
+}
+
+CNOID_IMPLEMENT_PLUGIN_ENTRY(ROS2Plugin)

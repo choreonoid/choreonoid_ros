8f0fcdf940570e4c62550d43ee77dd3a3d58e6aa
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-03-08 09:51:34 +0900

    Clean up unused header includes

--- src/node/choreonoid_ros2.cpp
+++ src/node/choreonoid_ros2.cpp
@@ -1,20 +1,11 @@
-#include "../plugin/ROS2Plugin.hpp"
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <cnoid/App>
-#include <cnoid/Config>
 #include <cnoid/ExecutablePath>
-#include <cnoid/FileUtil>
 #include <cnoid/Plugin>
 #include <cnoid/PluginManager>
 #include <cnoid/ProjectManager>
 #include <cnoid/UTF8>
 #include <cstdlib>
-#include <rclcpp/rclcpp.hpp>
-#include <regex>
-
-using namespace std;
-using namespace cnoid;
-namespace filesystem = stdx::filesystem;
 
 int main(int argc, char** argv)
 {
@@ -23,7 +14,7 @@ int main(int argc, char** argv)
     auto plugin_manager = cnoid::PluginManager::instance();
 
     if(auto pluginPath = getenv("CNOID_PLUGIN_PATH")){
-      plugin_manager->addPluginPath(toUTF8(pluginPath));
+      plugin_manager->addPluginPath(cnoid::toUTF8(pluginPath));
     }
 
     auto plugin_path = ament_index_cpp::get_package_share_directory("choreonoid_ros") + "/../../lib/choreonoid_ros";

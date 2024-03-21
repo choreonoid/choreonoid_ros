422489e88e68e131e507731a50645a48c450fa9a
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2024-03-21 21:30:46 +0900

    Fix the initialization of the additional plugin path in the choreonoid node for ROS 2

--- src/node/choreonoid_ros2.cpp
+++ src/node/choreonoid_ros2.cpp
@@ -1,16 +1,11 @@
-#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <ament_index_cpp/get_package_prefix.hpp>
 #include <rclcpp/utilities.hpp>
 
 #include <cnoid/App>
-#include <cnoid/ExecutablePath>
-#include <cnoid/Plugin>
 #include <cnoid/PluginManager>
-#include <cnoid/ProjectManager>
-#include <cnoid/UTF8>
 
 #include <string>
 #include <vector>
-#include <cstdlib>
 
 int main(int argc, char** argv)
 {
@@ -31,14 +26,8 @@ int main(int argc, char** argv)
 
     cnoid::App app(nonRosArgc, nonRosArgv, "Choreonoid-ROS2", "Choreonoid");
 
-    auto plugin_manager = cnoid::PluginManager::instance();
-
-    if(auto pluginPath = getenv("CNOID_PLUGIN_PATH")){
-      plugin_manager->addPluginPath(cnoid::toUTF8(pluginPath));
-    }
-
-    auto plugin_path = ament_index_cpp::get_package_share_directory("choreonoid_ros") + "/../../lib/choreonoid_ros";
-    plugin_manager->addPluginPath(plugin_path);
+    cnoid::PluginManager::instance()->addPluginDirectoryAsPrefix(
+        ament_index_cpp::get_package_prefix("choreonoid_ros"));
 
     app.requirePluginToCustomizeApplication("ROS2");
 
